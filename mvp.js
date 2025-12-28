c27ff408b7a86728ed44400b1277ab1974147087
OleggelO2002 <134651410+OleggelO2002@users.noreply.github.com>
2025-12-28 23:37:30 +0700

    Update mvp.js

--- mvp.js
+++ mvp.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (document.querySelector('.lesson-list')) {
-        document.body.style.backgroundImage = 'none !important';
-        document.body.style.backgroundColor = '#131313 !important';
+        document.body.style.setProperty('background-image', 'none', 'important');
+        document.body.style.setProperty('background-color', '#131313', 'important');
     }
 });
