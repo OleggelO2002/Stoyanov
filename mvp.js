473840fe222ec50ffc9b9e88c34e791974cc885c
OleggelO2002 <134651410+OleggelO2002@users.noreply.github.com>
2025-12-28 13:44:00 +0700

    Create mvp.js

---
+++ mvp.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (document.querySelector('.lesson-list')) {
+        document.body.style.backgroundImage = 'none';
+        document.body.style.backgroundColor = '#131313';
+    }
+});
