0506783a3cbb7ffad203ec120f5a84b7235a6d17
OleggelO2002 <134651410+OleggelO2002@users.noreply.github.com>
2026-03-10 01:45:48 +0700

    Update LK-admin.js

--- LK-admin.js
+++ LK-admin.js
@@ -1,1 +1,22 @@
-фыв
+document.addEventListener("DOMContentLoaded", function(){
+
+setTimeout(function(){
+
+document.querySelectorAll(".custom-field").forEach(function(field){
+
+const label = field.querySelector(".label-value");
+const description = field.querySelector(".field-description-block");
+
+if(!label || !description) return;
+
+const labelText = label.textContent.trim();
+const descriptionText = description.textContent.trim();
+
+label.textContent = descriptionText;
+description.textContent = labelText;
+
+});
+
+}, 500);
+
+});
